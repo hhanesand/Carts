582d35f05853cdb730d4cce515e9b6239ff196ec
Hakon Hanesand <hakon@hanesand.no>
2015-02-14 17:23:24 -0600

    fixed nil crash

---
+++ GroceryList/PFQueryTableViewController+GLMutableTableView.h
@@ -0,0 +1,13 @@
+//
+//  PFQueryTableViewController+GLMutableTableView.h
+//  GroceryList
+//
+//  Created by Hakon Hanesand on 2/14/15.
+//
+//
+
+#import "PFQueryTableViewController.h"
+
+@interface PFQueryTableViewController (GLMutableTableView)
+
+@end
---
+++ GroceryList/PFQueryTableViewController+GLMutableTableView.m
@@ -0,0 +1,13 @@
+//
+//  PFQueryTableViewController+GLMutableTableView.m
+//  GroceryList
+//
+//  Created by Hakon Hanesand on 2/14/15.
+//
+//
+
+#import "PFQueryTableViewController+GLMutableTableView.h"
+
+@implementation PFQueryTableViewController (GLMutableTableView)
+
+@end
